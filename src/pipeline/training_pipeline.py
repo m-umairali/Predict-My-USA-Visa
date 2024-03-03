836dbfb758e4590fd6bb1147ca3ca9be64965b72
umair <umairalism@gmail.com>
2024-03-03 15:03:14 +0500

    model trainer updated

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -5,18 +5,18 @@ from src.logger import logging
 from src.components.data_validation import DataValidation
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
-# from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainer
 
 from src.entity.config_entity import (DataIngestionConfig,
                                           DataValidationConfig,
-                                          DataTransformationConfig)
-                                        #   ModelTrainerConfig)
+                                          DataTransformationConfig,
+                                          ModelTrainerConfig)
                                           
 
 from src.entity.artifact_entity import (DataIngestionArtifact,
                                             DataValidationArtifact,
-                                            DataTransformationArtifact)
-                                            # ModelTrainerArtifact)
+                                            DataTransformationArtifact,
+                                            ModelTrainerArtifact)
 
 
 
@@ -25,7 +25,7 @@ class TrainPipeline:
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
         self.data_transformation_config = DataTransformationConfig()
-        # self.model_trainer_config = ModelTrainerConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
 
     
@@ -91,16 +91,16 @@ class TrainPipeline:
         
 
     
-    # def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
-    #     """
-    #     This method of TrainPipeline class is responsible for starting model training
-    #     """
-    #     try:
-    #         model_trainer = ModelTrainer(data_transformation_artifact=data_transformation_artifact,
-    #                                      model_trainer_config=self.model_trainer_config
-    #                                      )
-    #         model_trainer_artifact = model_trainer.initiate_model_trainer()
-    #         return model_trainer_artifact
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting model training
+        """
+        try:
+            model_trainer = ModelTrainer(data_transformation_artifact=data_transformation_artifact,
+                                         model_trainer_config=self.model_trainer_config
+                                         )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
 
         except Exception as e:
             raise USvisaException(e, sys)
@@ -121,7 +121,7 @@ class TrainPipeline:
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(
                 data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
-            # model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
 
 
         
